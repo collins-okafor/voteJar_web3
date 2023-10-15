c0a047802ff42524a43bfc98255a5a5bd7780d2b
Collins Okafor <collinszurum@gmail.com>
2023-10-15 13:03:05 +0100

    vote functions added

--- client-side/app/components/connected.jsx
+++ client-side/app/components/connected.jsx
@@ -6,7 +6,35 @@ const Connected = (props) =>
         <div className="login-container">
             <h1 className="connected-header">Connected to wallet</h1>
             <p className="connected-account">Wallet Account: { props.account}</p>
+            <p className="connected-account">Remaining Time: {props.remainingTime}</p>
+            
+            <div>
+                <input type="number" placeholder="Enter Candidate Index" value={props.number} onChange={props.handleNumberChange}></input>
+                <br/>
+                <button className="login-button" onClick={props.voteFunction}>Vote</button>
+            </div>
+
+            <table id="myTable" className="candidates-table">
+                <thead>
+                <tr>
+                    <th>Index</th>
+                    <th>Candidate name</th>
+                    <th>Candidate votes</th>
+                </tr>
+                </thead>
+                <tbody>
+                {props.candidates.map((candidate, index) => (
+                    <tr key={index}>
+                    <td>{candidate.index}</td>
+                    <td>{candidate.name}</td>
+                    <td>{candidate.voteCount}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
+
+        
     )
 }
 
--- client-side/app/src/App.jsx
+++ client-side/app/src/App.jsx
@@ -12,9 +12,16 @@ function App()
   const [account, setAccount] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
   const [votingStatus, setVotingStatus] = useState(true);
+  const [remainingTime, setRemainingTime] = useState('');
+  const [candidates, setCandidates] = useState([]);
+  const [number, setNumber] = useState('');
+  const [CanVote, setCanVote] = useState(true);
 
   useEffect(() =>
   {
+    getCandidates();
+    getRemainingTime();
+    getCurrentStatus();
     if (window.ethereum)
     {
       window.ethereum.on('accountsChanged', handleAccountsChanged);
@@ -29,6 +36,48 @@ function App()
     }
   })
 
+  async function vote()
+  {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+  }
+
+  async function canVote()
+  {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+    const voteStatus = await contractInstance.voters(await signer.getAddress());
+    setCanVote(voteStatus);
+  }
+
+  async function getCandidates()
+  {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    const contractInstance = new ethers.Contract(
+      contractAddress, contractAbi, signer
+    );
+    const candidatesList = await contractInstance.getAllVotesOfCandiates();
+    const formattedCandidates = candidatesList.map((candidate, index) =>
+    {
+      return {
+        index: index,
+        name: candidate.name,
+        voteCount: candidate.voteCount.toNumber()
+      }
+    });
+    setCandidates(formattedCandidates);
+  }
+
   async function getCurrentStatus()
   {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -38,10 +87,11 @@ function App()
       contractAddress, contractAbi, signer
     );
     const status = await contractInstance.getVotingStatus();
+    console.log(status);
     setVotingStatus(status);
   }
 
-  async function setRemainingTime()
+  async function getRemainingTime()
   {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     await provider.send('eth_requestAccounts', []);
@@ -49,14 +99,15 @@ function App()
     const contractInstance = new ethers.Contract(
       contractAddress, contractAbi, signer
     );
-    const status = await contractInstance.getVotingStatus();
-    setVotingStatus(status);
+    const time = await contractInstance.getRemainingTime();
+    setRemainingTime(parseInt(time, 16));
   }
 
   function handleAccountsChanged(accounts)
   {
     if (accounts.length > 0 && account !== accounts[0]) {
       setAccount(accounts[0]);
+      canVote();
     } else
     {
       setIsConnected(false);
@@ -78,7 +129,7 @@ function App()
         setAccount(address);
         console.log("Wallet Account Connected: " + address);
         setIsConnected(true);
-
+        canVote();
       } catch (err)
       {
         console.error(err);
@@ -89,9 +140,19 @@ function App()
     }
   }
 
+  async function handleNumberChange(e)
+  {
+    setNumber(e.target.value);
+  }
+
   return (
     <div className='App'>
-      {isConnected ? (<Connected account={account} />) : (<Login connectWallet={connectToWallet} />)}
+      {isConnected ? (<Connected
+        account={account}
+        candidates={candidates}
+        remainingTime={remainingTime}
+        number={number}
+        handleNumberChange={handleNumberChange} />) : (<Login connectWallet={connectToWallet} />)}
       
     </div>
   )
